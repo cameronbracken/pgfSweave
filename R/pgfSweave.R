d41344e76043222a6b4cdcd48321b46d41b34a59
Cameron Bracken <cameron.bracken@gmail.com>
2009-03-10 06:58:56 +0000

    - updated more robust example that actually compiles - fix to pgfSweave.R to handle file names with absolute paths - small documentation changes

--- R/pgfSweave.R
+++ R/pgfSweave.R
@@ -7,15 +7,18 @@ pgfSweave <- function(file, compile.tex = TRUE, syntax = getOption("SweaveSyntax
     if(compile.tex){
         
         #Strip the extension and compile the pgf graphics separately
-	    fn <- strsplit(file,'\\.')[[1]][1]
+        
+	    bn <- strsplit(basename(file),"\\.Rnw")[[1]][1]
+        dn <- dirname(file)
+        fn <- file.path(dn,bn)
         cmds <- readLines(paste(fn,'sh',sep='.'))
         dummy <- lapply(cmds,system)
 
         #set special versions of calls to latex or pdflatex
         if(is.null(match.call()$pdf))
-            Sys.setenv(LATEX=paste("latex --jobname=",fn,sep=''))
+            Sys.setenv(LATEX=paste("latex --jobname=",bn,sep=''))
         else
-            Sys.setenv(PDFLATEX=paste("pdflatex --jobname=",fn,sep=''))
+            Sys.setenv(PDFLATEX=paste("pdflatex --jobname=",bn,sep=''))
     
         #run texi2dvi on the tex file        
         tools::texi2dvi(paste(fn,'tex',sep='.'),...)
